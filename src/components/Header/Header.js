75ea94b50dd560198627982caa4d6b2cc24c158a
esin87 <esin.saribudak@gmail.com>
2020-04-27 14:55:10 -0500

    Add and style Header and navigation

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
-	return <h1>Covid-19 Data</h1>;
+	return (
+		<Navbar bg='light' variant='light'>
+			<Navbar.Brand>Covid-19 Data</Navbar.Brand>
+			<Navbar.Collapse id='basic-navbar-nav'>
+				<Nav>
+					<LinkContainer to='/'>
+						<Nav.Link>Home</Nav.Link>
+					</LinkContainer>
+					<LinkContainer to='/search'>
+						<Nav.Link>Country Search</Nav.Link>
+					</LinkContainer>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 export default Header;
