8225c497f6c80ad14ce8dcfd0cae85e0f4106c6a
esin87 <esin.saribudak@gmail.com>
2020-04-27 18:09:49 -0500

    Updates

---
+++ precache-manifest.7b752ea6346c7a00502bab8d6325d4d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5265c4c4878b3eb100fdd5add7d6fb65",
+    "url": "/covid19-data/index.html"
+  },
+  {
+    "revision": "72739bf160d07e76e747",
+    "url": "/covid19-data/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8349314337233ecef260",
+    "url": "/covid19-data/static/css/main.1bff27a9.chunk.css"
+  },
+  {
+    "revision": "72739bf160d07e76e747",
+    "url": "/covid19-data/static/js/2.e625d286.chunk.js"
+  },
+  {
+    "revision": "005c97f82cee60e20eac659314ea29ec",
+    "url": "/covid19-data/static/js/2.e625d286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8349314337233ecef260",
+    "url": "/covid19-data/static/js/main.9371383d.chunk.js"
+  },
+  {
+    "revision": "891fc82c7a6a746ebe07",
+    "url": "/covid19-data/static/js/runtime-main.1ca7bf56.js"
+  }
+]);
